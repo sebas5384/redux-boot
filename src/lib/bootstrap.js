b7dfa518bbe42b86ec07bd1a3463f4e55a0b8063
Henrique Recidive <recidive@gmail.com>
2016-04-13 23:14:27 -0300

    Factored out store creation part to configureStore lib.

--- src/lib/bootstrap.js
+++ src/lib/bootstrap.js
@@ -1,29 +1,22 @@
-import {createStore, applyMiddleware} from 'redux'
 import {createAction} from 'redux-actions'
-import promiseMiddleware from 'redux-promise'
-import combineReducers from './combine-reducers'
 import processModules from './process-modules'
+import configureStore from './configure-store'
 
 export const BOOT = 'choko/core/BOOT'
 
 export default function bootstrap(initialState = {}, modules = []) {
   const {reducers, middlewares} = processModules(modules)
 
-  // Creates the root middleware and adds redux-promise middleware.
-  const rootMiddleware = [promiseMiddleware].concat(middlewares)
+  let store = configureStore(initialState, reducers, middlewares)
 
-  const rootReducer = combineReducers(reducers)
-
-  let store = createStore(rootReducer, initialState, applyMiddleware(...rootMiddleware))
-
-  const bootPromise = store.dispatch(bootAction(initialState))
-
-  return bootPromise.then(action => {
-    return {
-      action,
-      store
-    }
-  })
+  return store
+    .dispatch(bootAction(initialState))
+    .then(action => {
+      return {
+        action,
+        store
+      }
+    })
 }
 
 export const bootAction = createAction(BOOT, async initialState => initialState)
---
+++ src/lib/configure-store.js
@@ -0,0 +1,11 @@
+import {createStore, applyMiddleware} from 'redux'
+import promiseMiddleware from 'redux-promise'
+import combineReducers from './combine-reducers'
+
+export default function configureStore(initialState, reducers, middlewares) {
+
+  const rootMiddleware = [promiseMiddleware].concat(middlewares)
+  const rootReducer = combineReducers(reducers)
+
+  return createStore(rootReducer, initialState, applyMiddleware(...rootMiddleware))
+}
